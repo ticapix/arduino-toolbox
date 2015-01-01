9429f56c2754e0aa3071521138c4204d087c6cca
Pierre Gr <ticapix@gmail.com>
2015-01-01 13:43:21 +0200

    initial commit

---
+++ ring_buffer.h
@@ -0,0 +1,93 @@
+#ifndef __STRING_BUFFER_H__
+#define __STRING_BUFFER_H__
+
+template<uint16_t Size, typename T>
+class RingBuffer {
+
+  public:
+    RingBuffer()
+      : _start(0),
+        _end(0),
+        _length(0),
+        _capacity(Size)
+    { }
+
+    bool append(T c) {
+      if (full())
+        return false;
+      _buffer[_end] = c;
+      _end = (_end + 1) % _capacity;
+      ++_length;
+      return true;
+    }
+
+    bool full() {
+      return _capacity == _length;
+    }
+
+    bool empty() {
+      return _length == 0;
+    }
+
+    void clear() {
+      _start = 0;
+      _end = 0;
+      _length = 0;
+    }
+
+    uint16_t length() {
+      return _length;
+    }
+
+    const /* can't modify the returned value */ T operator[](uint16_t idx) const { /* no modification inside this fct */
+      if (idx > _length)
+        return (*this)[0]; // declared as undefined behavior for the user
+      return _buffer[(_start + idx) % _capacity];
+    }
+
+    T pop_first() {
+      uint8_t t = _start;
+      _start = (_start + 1) % _capacity;
+      --_length;
+      return _buffer[t];
+    }
+    
+
+  protected:
+    T _buffer[Size];
+    uint16_t _start, _end;
+    uint16_t _length;
+    uint16_t _capacity;
+};
+
+template<uint16_t Size>
+class StringBuffer : public RingBuffer <Size, byte> {
+  public:
+  bool appendS(String str) {
+    for (uint16_t i = 0; i < str.length(); ++i) {
+      if (!this->append(str[i])) {
+	return false;
+      }
+    }
+    return true;
+  }
+  
+    int indexOf(String substr, uint16_t offset = 0) {
+      if (offset > this->length())
+        return -1;
+      if (substr.length() + offset > this->length())
+        return -1;
+      for (uint16_t i = offset; i <= this->length() - substr.length(); ++i) {
+        bool match = true;
+        for (uint16_t j = 0; j < substr.length() && match; ++j) {
+          match = match && ((*this)[i + j] == substr[j]);
+        }
+        if (match)
+          return i;
+      }
+      return -1;
+    }
+};
+
+
+#endif
---
+++ test/test.cpp
@@ -0,0 +1,122 @@
+#include <gtest/gtest.h>
+
+#include <unistd.h>
+#include <stdio.h>
+#include <stdint.h>
+#include <string>
+#include <assert.h>
+
+typedef uint8_t byte;
+typedef std::string String;
+
+#define PRINT_BUFFER(b) { for (uint16_t i = 0; i < b.length(); ++i) { \
+      printf("%c", b[i]);					      \
+    }								      \
+    printf("\n");						      \
+  }
+
+#include "../ring_buffer.h"
+
+TEST(StringBuffer, append) {
+  StringBuffer<2> buff;
+  assert(buff.indexOf("1") == -1);
+  assert(buff.append('1') == true);
+  assert(buff.indexOf("1") == 0);
+}
+
+TEST(StringBuffer, pop) {
+  StringBuffer<2> buff;
+  assert(buff.length() == 0);
+  assert(buff.append('1') == true);
+  assert(buff.length() == 1);
+  assert(buff.append('2') == true);
+  assert(buff.length() == 2);
+  assert(buff.append('3') == false);
+  assert(buff.pop_first() == '1');
+  assert(buff.length() == 1);
+  assert(buff.pop_first() == '2');
+  assert(buff.length() == 0);
+}
+
+TEST(StringBuffer, accessor) {
+  StringBuffer<3> buff;
+  assert(buff.append('1') == true);
+  assert(buff.append('2') == true);
+  assert(buff.append('3') == true);
+  PRINT_BUFFER(buff);
+  assert(buff.length() == 3);
+  assert(buff[0] == '1');
+  assert(buff[1] == '2');
+  assert(buff[2] == '3');
+  assert(buff.pop_first() == '1');
+  assert(buff.append('4') == true);
+  assert(buff[2] == '4');    
+  assert(buff.length() == 3);
+  assert(buff.append('5') == false);
+  assert(buff.pop_first() == '2');
+  assert(buff.pop_first() == '3');
+  assert(buff.length() == 1);
+  PRINT_BUFFER(buff);
+}
+
+TEST(StringBuffer, indexOf) {
+  StringBuffer<10> buff;
+  for (auto c: {'1', '2', '3', '4', 'O', 'K', 'A', 'Y', '5', '6'})
+    assert(buff.append(c) == true);
+  PRINT_BUFFER(buff);
+  assert(buff.indexOf("1") == 0);
+  assert(buff.indexOf("2") == 1);
+  assert(buff.indexOf("12") == 0);
+  assert(buff.indexOf("23") == 1);
+  assert(buff.indexOf("34") == 2);
+  assert(buff.indexOf("OKAY") == 4);
+  assert(buff.pop_first() == '1');
+  assert(buff.pop_first() == '2');
+  assert(buff.indexOf("OKAY") == 2);
+  {
+    uint16_t pos = buff.indexOf("OKAY") + 4;
+    while (pos--)
+      buff.pop_first();
+    PRINT_BUFFER(buff);
+    assert(buff.length() == 2);
+  }
+  assert(buff.indexOf("56") == 0);
+}
+
+TEST(StringBuffer, indexOf_offset) {
+  StringBuffer<10> buff;
+  for (auto c: {'1', '2', '3', '1', '2', '3', '1', '2', '3', '1'})
+    assert(buff.append(c) == true);
+  PRINT_BUFFER(buff);
+  int pos = 0;
+  uint16_t count = 0;
+  while (true) {
+    pos = buff.indexOf("1", pos);
+    if (pos == -1)
+      break;
+    assert(buff[pos] == '1');
+    ++pos;
+    ++count;
+  }
+  assert(count == 4);
+}
+
+TEST(StringBuffer, full_empty) {
+  StringBuffer<1> buff;
+  assert(buff.empty() == true);
+  buff.append('1');
+  assert(buff.full() == true);
+  buff.clear();
+  assert(buff.empty() == true);
+}
+
+TEST(StringBuffer, append_string) {
+  StringBuffer<3> buff;
+  buff.appendS("123");
+  assert(buff.full() == true);
+}
+
+int main(int argc, char* argv[]) {
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
