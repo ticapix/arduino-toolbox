ee2b7ddf2ceab282fb2eee017c8492b9e2bfd46e
Pierre Gr <ticapix@gmail.com>
2015-01-06 15:55:24 +0200

    update tests with new append signature

--- tests/test-RingBuffer.cpp
+++ tests/test-RingBuffer.cpp
@@ -77,16 +77,16 @@ TEST(StringBuffer, pop_last) {
 
 TEST(StringBuffer, accessor) {
 	StringBuffer<3> buff;
-	ASSERT_TRUE(buff.append("123"));
+	ASSERT_EQ(buff.append("123"), 3);
 	ASSERT_EQ(buff.length(), 3);
 	ASSERT_EQ(buff[0], '1');
 	ASSERT_EQ(buff[1], '2');
 	ASSERT_EQ(buff[2], '3');
 	ASSERT_EQ(buff.pop_first(), '1');
-	ASSERT_TRUE(buff.append('4'));
+	ASSERT_EQ(buff.append('4'), 1);
 	ASSERT_EQ(buff[2], '4');
 	ASSERT_EQ(buff.length(), 3);
-	ASSERT_FALSE(buff.append('5'));
+	ASSERT_EQ(buff.append('5'), 0);
 	ASSERT_EQ(buff.pop_first(), '2');
 	ASSERT_EQ(buff.pop_first(), '3');
 	ASSERT_EQ(buff.length(), 1);
@@ -100,7 +100,7 @@ TEST(StringBuffer, index_of_not_found) {
 TEST(StringBuffer, index_of) {
 	StringBuffer<10> buff;
 	for (auto c : { '1', '2', '3', '4', 'O', 'K', 'A', 'Y', '5', '6' })
-		ASSERT_TRUE(buff.append(c));
+	        ASSERT_EQ(buff.append(c), 1);
 	ASSERT_EQ(buff.index_of("1"), 0);
 	ASSERT_EQ(buff.index_of("2"), 1);
 	ASSERT_EQ(buff.index_of("12"), 0);
@@ -122,7 +122,7 @@ TEST(StringBuffer, index_of) {
 TEST(StringBuffer, index_of_offset) {
 	StringBuffer<10> buff;
 	for (auto c : { '1', '2', '3', '1', '2', '3', '1', '2', '3', '1' })
-		ASSERT_TRUE(buff.append(c));
+	        ASSERT_EQ(buff.append(c), 1);
 	int pos = 0;
 	uint16_t count = 0;
 	while (true) {
@@ -163,9 +163,9 @@ TEST(StringBuffer, buffer) {
 TEST(RingBuffer, memmove_with_int) {
 	RingBuffer<3, int> buff;
 	for (auto c : { 1, 2, 3 })
-		ASSERT_TRUE(buff.append(c));
+	        ASSERT_EQ(buff.append(c), 1);
 	buff.pop_first();
-	ASSERT_TRUE(buff.append(4)); // should not be continous anymore in internal mem
+	ASSERT_EQ(buff.append(4), 1); // should not be continous anymore in internal mem
 	ASSERT_EQ(buff.length(), 3);
 	ASSERT_EQ(buff[0], 2);
 	ASSERT_EQ(buff[1], 3);
@@ -178,17 +178,16 @@ TEST(RingBuffer, memmove_with_int) {
 
 TEST(RingBuffer, memmove_append) {
 	RingBuffer<3, short> buff;
-	for (auto c : { 1, 2, 3 })
-		ASSERT_TRUE(buff.append(c)); // |1|2|3|
+        ASSERT_EQ(buff.append("123"), 3); // |1|2|3|
 	buff.pop_first(); // | |2|3|
 	buff.pop_first(); // | | |3|
+	ASSERT_EQ(buff.append(4), 1); // |4| |3|
 	// should not be continous anymore in internal mem
-	ASSERT_TRUE(buff.append(4)); // |4| |3|
 	ASSERT_EQ(buff.length(), 2);
 	// should trigger an internal memmove
 	buff.buffer(); // | |3|4|
 	// check that indexes are correct avec memmove
-	ASSERT_TRUE(buff.append(5)); // |5|3|4|
+	ASSERT_EQ(buff.append(5), 1); // |5|3|4|
 	// should trigger an internal memmove
 	const short *arr = buff.buffer(); // |3|4|5|
 	ASSERT_EQ(arr[0], 3);
