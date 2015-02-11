b04e43c5ca243afe893d80caa6c2f0755beac5d9
ticapix <ticapix@gmail.com>
2015-02-11 02:52:15 +0200

    add coroutine tests

--- Coroutine/Coroutine.h
+++ Coroutine/Coroutine.h
@@ -8,18 +8,21 @@ unsigned long millis();
 class ICoroutine {
 public:
 	ICoroutine(unsigned long timeout_ms) :
-		_live(true),
-		_state(0),
-		_has_timeout(false),
-		_start(millis()),
-		_timeout_ms(timeout_ms),
-		_subtask(nullptr) {};
+			_live(true), _state(0), _has_timeout(false), _start(millis()), _timeout_ms(
+					timeout_ms), _subtask(nullptr) {
+	}
 
-	bool live() { return _live; };
+	bool live() {
+		return _live;
+	}
 
-	virtual void run() { _live = false; };
+	virtual void run() {
+		_live = false;
+	}
 
-	bool has_timeout() { return _has_timeout; };
+	bool has_timeout() {
+		return _has_timeout;
+	}
 
 	virtual ~ICoroutine() {
 		if (_subtask != nullptr) {
@@ -36,54 +39,104 @@ protected:
 	ICoroutine* _subtask;
 };
 
+#define COROUTINE(return_type, class_name, source) 									\
+class class_name : public ICoroutine { 												\
+public: 																			\
+	class_name(unsigned long timeout_ms = TIMEOUT_MS) : ICoroutine(timeout_ms) {} 	\
+	return_type result() { return _result; } 										\
+	virtual void run(); 															\
+private: 																			\
+	return_type _result;															\
+	source																			\
+};
 
-#define COROUTINE(return_type, class_name) \
-class class_name : public ICoroutine { \
-public: \
-	class_name(unsigned long timeout_ms = TIMEOUT_MS) : ICoroutine(timeout_ms) {} \
-	return_type result() { return _result; } \
-private: \
-	return_type _result;
-
-#define CORO_ARG(class_name, arg_type, arg_name) \
-public: \
+#define CORO_ARG(class_name, arg_type, arg_name) 													\
+public: 																							\
 	class_name* set_##arg_name(arg_type _##arg_name) { this->arg_name = _##arg_name; return this; } \
-private: \
+private: 																							\
 arg_type arg_name;
 
-#define CORO_VAR(var_type, var_name) \
-private: \
+#define CORO_VAR(var_type, var_name)	\
+private: 								\
 	var_type var_name;
 
-#define CORO_START() \
-	void run() { \
-if (millis() - _start > _timeout_ms) { _has_timeout = true; _live = false; } \
-if (!_live) return; \
-	switch (_state) { \
+#define CORO_START(class_name)	 													\
+void class_name::run() { 															\
+	if (millis() - _start > _timeout_ms) { _has_timeout = true; _live = false; }	\
+	if (!_live) return; 															\
+	switch (_state) { 																\
 	case 0:;
 
-#define CORO_END() \
-	_live = false; \
-	return; \
-} \
-	} \
-};
+#define CORO_END()	\
+	_live = false;	\
+	return; 		\
+	}				\
+}
 
 #define CORO_RETURN(result) _result = result;
 
 #define YIELD() { _state = __LINE__; return; case __LINE__:; }
 
-#define AWAIT(coro) \
-if (_subtask != nullptr) delete _subtask; \
-	_subtask = coro; \
-while (_subtask->live()) { \
-	_subtask->run(); \
-	YIELD(); \
+#define AWAIT(coro) 						\
+if (_subtask != nullptr) delete _subtask; 	\
+_subtask = coro; 							\
+while (_subtask->live()) { 					\
+	_subtask->run(); 						\
+	YIELD(); 								\
 }
 
-#define RESULT(class_name) \
-	[this](){auto tmp = ((class_name*)this->_subtask)->result(); delete this->_subtask; this->_subtask = nullptr; return tmp; }()
+#define RESULT(class_name) 								\
+[this](){												\
+	if (this->_subtask == nullptr)						\
+		return _result;									\
+	auto tmp = ((class_name*)this->_subtask)->result();	\
+	delete this->_subtask;								\
+	this->_subtask = nullptr;							\
+	return tmp;											\
+}()
 
 #define HAS_TIMEOUT() _subtask == nullptr ? false : _subtask->has_timeout()
+
+//////////// EXAMPLE OF MAIN SCHEDULING LOOP implementation ////////////
+/*
+ * It can be the only thing you have the call from the arduino loop() function
+ */
+
+typedef void (*coro_callback)(uint8_t idx, const ICoroutine*);
+
+void schedule_coro(ICoroutine* coroutines[], uint8_t size, coro_callback clbk) {
+	bool has_coro = true;
+	while (has_coro) {
+		has_coro = false;
+		for (uint8_t idx = 0; idx < size; ++idx) {
+			ICoroutine* coro = coroutines[idx];
+			if (coro == nullptr)
+				continue;
+			has_coro = true;
+			if (coro->live()) {
+				coro->run();
+			} else {
+				clbk(idx, coro);
+				delete coro;
+				coroutines[idx] = nullptr;
+			}
+		}
+	}
+}
+
+//////////// EXAMPLE OF ASYNC Delay implementation ////////////
+
+/*
+ * This coroutine will simply exit when timing out
+ */
+
+COROUTINE(int, Delay,) // NOTE the ', ' because there is no argument or variable
+CORO_START(Delay)
+{
+	while (true)
+		YIELD()
+}
+CORO_RETURN(0)
+CORO_END()
 
 #endif
--- tests/test-Coroutine.cpp
+++ tests/test-Coroutine.cpp
@@ -1,42 +1,112 @@
 #include <gtest/gtest.h>
+#include <gmock/gmock.h>
+#include <memory>
 
 #include <Arduino.h>
 #include <Coroutine.h>
 
+using ::testing::_;
+using ::testing::Field;
+using ::testing::Pointee;
+using ::testing::Property;
+using ::testing::InSequence;
+using ::testing::SafeMatcherCast;
+using ::testing::Invoke;
+using ::testing::WithArg;
+using ::testing::Unused;
 
-defCoroutine(int, sequence1) {
-  int i;
-  cBegin();
-  for (i = 0; i < 5; i += 2) {
-    cYield( i );
-    cYield( i+1 );
-  }
-  cEnd(-1);
+
+class SchedulerClkMock {
+public:
+	MOCK_METHOD2(callback, void(uint8_t idx, const ICoroutine*));
 };
 
-defCoroutine(int, sequence) {
-  int i, j;
-  sequence1 s1;
-  
-  cBegin(); // TODO put extra arg in the cBegin() macro
-  // cYield(s1)
-  
-  while(cResume(j, s1))
-    cYield(j);
-  for (i = 0; i < 5; i += 2) {
-    cYield(i);
-    cYield(i+1);
-  }
-  cEnd(-1);
-};
-
-TEST(Coroutine, simple) {
-  sequence corout1;
-
-  int n;
-  while (cResume(n, corout1)) {
-    printf("%d\n", n);
-    delay(10);
-  }
+SchedulerClkMock* g_sched;
+void clk_forward(uint8_t idx, const ICoroutine* coro) {
+	g_sched->callback(idx, coro);
 }
 
+/////////////////////////////////////////////////////////////////////
+
+TEST(Coroutine, simple_delay) {
+	g_sched = new SchedulerClkMock();
+	ICoroutine* coroutines[] = {new Delay(100)};
+
+	EXPECT_CALL(*g_sched, callback(0, _));
+	schedule_coro(coroutines, sizeof (coroutines) / sizeof(coroutines[0]), clk_forward);
+	delete g_sched;
+}
+
+TEST(Coroutine, two_delay) {
+	g_sched = new SchedulerClkMock();
+	ICoroutine* coroutines[] = {new Delay(100), new Delay(150), new Delay(50)};
+
+	InSequence s;
+	EXPECT_CALL(*g_sched, callback(2, _));
+	EXPECT_CALL(*g_sched, callback(0, _));
+	EXPECT_CALL(*g_sched, callback(1, _));
+	schedule_coro(coroutines, sizeof (coroutines) / sizeof(coroutines[0]), clk_forward);
+	delete g_sched;
+}
+
+/////////////////////////////////////////////////////////////////////
+
+COROUTINE(int, CountNumber,
+	CORO_ARG(CountNumber, int, limit)
+	CORO_VAR(int, count)
+)
+CORO_START(CountNumber);
+{
+	count = 0;
+	while (count < limit) {
+		++count;
+		YIELD();
+	}
+}
+CORO_RETURN(count);
+CORO_END();
+
+TEST(Coroutine, two_counters) {
+	g_sched = new SchedulerClkMock();
+	ICoroutine* coroutines[] = {(new CountNumber())->set_limit(10), (new CountNumber())->set_limit(5)};
+
+	InSequence s;
+	// TODO: replace with WhenDynamicCastTo when available
+	EXPECT_CALL(*g_sched, callback(1, SafeMatcherCast<const ICoroutine*>(coroutines[1]))).WillOnce(Invoke(
+		[](Unused, const ICoroutine* coro) {ASSERT_EQ(5, ((CountNumber*)coro)->result());}
+	));
+	EXPECT_CALL(*g_sched, callback(0, SafeMatcherCast<const ICoroutine*>(coroutines[0]))).WillOnce(Invoke(
+		[](Unused, const ICoroutine* coro) {ASSERT_EQ(10, ((CountNumber*)coro)->result());}
+	));
+	schedule_coro(coroutines, sizeof (coroutines) / sizeof(coroutines[0]), clk_forward);
+	delete g_sched;
+}
+/////////////////////////////////////////////////////////////////////
+
+COROUTINE(int, CountChar,
+	CORO_ARG(CountChar, char, from)
+	CORO_ARG(CountChar, char, to)
+)
+CORO_START(CountChar);
+{
+	AWAIT(new Delay(50 * (to - from)));
+	ASSERT_TRUE(HAS_TIMEOUT());
+	AWAIT((new CountNumber())->set_limit(to - from));
+	ASSERT_EQ(to - from, RESULT(CountNumber));
+}
+CORO_RETURN(0);
+CORO_END();
+
+TEST(Coroutine, two_countchar) {
+	g_sched = new SchedulerClkMock();
+	ICoroutine* coroutines[] = {
+			(new CountChar())->set_from('A')->set_to('B'),
+			(new CountChar())->set_from('B')->set_to('F')};
+
+	InSequence s;
+	// TODO: replace with WhenDynamicCastTo when available
+	EXPECT_CALL(*g_sched, callback(0, SafeMatcherCast<const ICoroutine*>(coroutines[0])));
+	EXPECT_CALL(*g_sched, callback(1, SafeMatcherCast<const ICoroutine*>(coroutines[1])));
+	schedule_coro(coroutines, sizeof (coroutines) / sizeof(coroutines[0]), clk_forward);
+	delete g_sched;
+}
